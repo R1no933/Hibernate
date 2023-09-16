6aab28cf0cf0cd8611b9731d6b7901b3a1e37252
R1no933 <dbaskakov93@yandex.ru>
2023-09-16 16:33:46 +0300

    add User entity for mapping, add user in db with hiber session

--- src/main/java/hibernate/HibernateRunner.java
+++ src/main/java/hibernate/HibernateRunner.java
@@ -1,16 +1,31 @@
 package hibernate;
 
+import hibernate.entity.User;
 import org.hibernate.*;
 import org.hibernate.cfg.Configuration;
+
+import java.time.LocalDate;
 
 public class HibernateRunner {
     public static void main(String[] args) {
         Configuration configuration = new Configuration();
+        configuration.addAnnotatedClass(User.class);
         configuration.configure();
 
         try (SessionFactory sessionFactory = configuration.buildSessionFactory();
             Session session = sessionFactory.openSession()) {
-            System.out.println("Session create! All ok!");
+            session.beginTransaction();
+
+            User user = User.builder()
+                    .username("r1n0")
+                    .firstname("Dmitriy")
+                    .lastname("Baskakov")
+                    .birthDate(LocalDate.of(1993, 03, 19))
+                    .age(30)
+                    .build();
+            session.save(user);
+
+            session.getTransaction().commit();
         }
     }
 }
