b63b945a1127df686a86b7207b229b2d011d7713
R1no933 <dbaskakov93@yandex.ru>
2023-09-22 20:03:12 +0300

    add interceptor and bytebuddy user services

--- src/main/java/hibernate/HibernateRunner.java
+++ src/main/java/hibernate/HibernateRunner.java
@@ -7,16 +7,22 @@ import hibernate.dto.UserCreateDto;
 import hibernate.entity.PersonalInfo;
 import hibernate.entity.Role;
 import hibernate.entity.User;
+import hibernate.interceptor.TransactionalInterceptor;
 import hibernate.mapper.CompanyReadMapper;
 import hibernate.mapper.UserCreateMapper;
 import hibernate.mapper.UserReadMapper;
 import hibernate.service.UserService;
 import hibernate.util.HibernateUtil;
 import lombok.extern.slf4j.Slf4j;
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.implementation.MethodDelegation;
+import net.bytebuddy.matcher.ElementMatchers;
 import org.hibernate.*;
 
 import javax.transaction.Transactional;
+import java.lang.InstantiationException;
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.time.LocalDate;
@@ -24,12 +30,12 @@ import java.time.LocalDate;
 @Slf4j
 public class HibernateRunner {
     @Transactional
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory()) {
 
             Session session = (Session) Proxy.newProxyInstance(SessionFactory.class.getClassLoader(), new Class[]{Session.class},
                     (proxy, method, args1) -> method.invoke(sessionFactory.getCurrentSession(), args1));
-            session.beginTransaction();
+            //session.beginTransaction();
 
             CompanyReadMapper companyReadMapper = new CompanyReadMapper();
             CompanyRepository companyRepository = new CompanyRepository(session);
@@ -37,9 +43,20 @@ public class HibernateRunner {
             UserRepository userRepository = new UserRepository(session);
             UserCreateMapper userCreateMapper = new UserCreateMapper(companyRepository);
             PaymentRepository paymentRepository = new PaymentRepository(session);
-            UserService userService = new UserService(userRepository, userReadMapper, userCreateMapper);
+            //UserService userService = new UserService(userRepository, userReadMapper, userCreateMapper);
+            TransactionalInterceptor interceptor = new TransactionalInterceptor(sessionFactory);
 
-            userService.findById(1L).ifPresent(System.out::println);
+            UserService byteBuddyUserService = new ByteBuddy()
+                    .subclass(UserService.class)
+                    .method(ElementMatchers.any())
+                    .intercept(MethodDelegation.to(interceptor))
+                    .make().load(UserService.class.getClassLoader())
+                    .getLoaded()
+                    .getDeclaredConstructor(UserRepository.class, UserReadMapper.class, UserCreateMapper.class)
+                    .newInstance(userRepository, userReadMapper, userCreateMapper);
+
+            //userService.findById(1L).ifPresent(System.out::println);
+            byteBuddyUserService.findById(1L).ifPresent(System.out::println);
 
             UserCreateDto UserCreateDto = new UserCreateDto(
                     PersonalInfo.builder()
@@ -52,9 +69,10 @@ public class HibernateRunner {
                     Role.ADMIN,
                     1
             );
-            userService.create(UserCreateDto);
+            byteBuddyUserService.create(UserCreateDto);
+            //userService.create(UserCreateDto);
 
-            session.getTransaction().commit();
+            //session.getTransaction().commit();
         }
     }
 }
